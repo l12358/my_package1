42a8cef88c4e7ec61c899c716e0f2c1caf2f7eb7
l12358 <laura@pmap.ca>
2025-12-30 22:27:21 -0800

    second commit

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='demo_nodes_cpp',
-            executable='talker'
+            package='demo_nodes_py',
+            executable='listener'
         )
     ])
